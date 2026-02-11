ddb9a21986d5476376770079c1c158479795414e
Kazem Ardaneh <kardaneh@espri-calcul-01.ipsl.fr>
2026-02-11 11:30:23 +0100

    add the full hydrol to py transform

--- run_isolator.sh
+++ run_isolator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "Starting isolation: 20260209_084746"
+echo "Starting isolation: 20260209_095209"
 echo "Target module: hydrol"
 echo "Parent subroutine: hydrol_main"
 echo "Target subroutines: hydrol_alma hydrol_vegupd hydrol_canop hydrol_flood hydrol_hydraulic_arch_tuzet_calc hydrol_soil explicitsnow_main"
