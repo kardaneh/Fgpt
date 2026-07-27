f7a4e7cc7d54abfc8e0a407b4b6b695fa7edb37e
Kazem Ardaneh <kardaneh@espri-calcul-01.ipsl.fr>
2026-07-27 22:59:45 +0200

    refactore: add some files into gitign

--- run_isolator.sh
+++ run_isolator.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-echo "Starting isolation: 20260716_143056"
+echo "Starting isolation: 20260723_083956"
 echo "Target module: hydrol"
 echo "Parent subroutine: hydrol_main"
 echo "Target subroutines: hydrol_alma hydrol_vegupd hydrol_canop hydrol_flood hydrol_hydraulic_arch_tuzet_calc hydrol_soil explicitsnow_main"
-echo "OpenACC: False, F2PY: True, Tapenade: False", PY2JX: True
+echo "OpenACC: False, F2PY: True, Tapenade: False", PY2JX: False
 echo "=========================================="
 fgpt isolate \
-    --work "/data/ssivanes" \
+    --work "/scratchu/kardaneh" \
     --rest_of_path "modipsl_truck_opt/modeles/ORCHIDEE/src_sechiba/" \
     --target_module "hydrol" \
     --parent_subroutine "hydrol_main" \
@@ -14,8 +14,7 @@ fgpt isolate \
     --openacc "False" \
     --f2py "True" \
     --tapenade "False" \
-    --py2jx "True" \
-    --mode "jax" \
+    --py2jx "False" \
     --config_path "template.yaml" \
     --vectorize kjpindex \
     --mode "jax" \
