5bfdaed8ba3799bf5142161470b8636de027c345
kardaneh <kazem.ardaneh@ipsl.fr>
2026-09-15 11:47:01 +0200

    refact: setup file, and run for the edited code

--- run_isolator.sh
+++ run_isolator.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-echo "Starting isolation: 20260911_160617"
-echo "Target module: hydrol"
-echo "Parent subroutine: "
-echo "Target subroutines: hydrol_alma hydrol_vegupd hydrol_canop hydrol_flood hydrol_hydraulic_arch_tuzet_calc hydrol_soil explicitsnow_main"
+echo "Starting isolation: 20260915_113130"
+echo "Target module: condveg"
+echo "Parent subroutine: condveg_main"
+echo "Target subroutines: albedo_surface_main"
 echo "OpenACC: False, F2PY: False, Tapenade: True", PY2JX: False
 echo "=========================================="
 fgpt isolate \
     --work "/scratchu/kardaneh" \
     --rest_of_path "tmp/modipsl_truck_opt/modeles/ORCHIDEE/src_sechiba/" \
     --target_model "ORCHIDEE" \
-    --target_module "hydrol" \
-    --parent_subroutine "" \
-    --target_subroutines hydrol_alma hydrol_vegupd hydrol_canop hydrol_flood hydrol_hydraulic_arch_tuzet_calc hydrol_soil explicitsnow_main \
+    --target_module "condveg" \
+    --parent_subroutine "condveg_main" \
+    --target_subroutines albedo_surface_main \
     --openacc "False" \
-    --f2py "True" \
-    --tapenade "False" \
+    --f2py "False" \
+    --tapenade "True" \
     --py2jx "False" \
     --config_path "template.yaml" \
     --vectorize kjpindex \
