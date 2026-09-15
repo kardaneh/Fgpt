e1f0df0632d35ee27b28e5b6e02c9e93938b9dfe
kardaneh <kazem.ardaneh@ipsl.fr>
2026-09-15 11:45:32 +0200

    feat: python examples files added. These will be served as the integeration tests later!

---
+++ examples/example_01.py
@@ -0,0 +1,75 @@
+import sys
+
+from fgpt import Isolator
+from fgpt.core.common import Logger
+
+# Fgpt Imports
+from fgpt.core.frontend import Processor
+from fgpt.isolator import parse_args
+
+# Initialize Fgpt
+logger = Logger()
+processor = Processor(logger=logger)
+
+# Define the options
+sys.argv = [
+    "my_program",
+    "--rest_of_path",
+    "Fgpt/examples",
+    "--target_module",
+    "example_01",
+    "--work",
+    "/home/kardaneh",
+    "--target_model",
+    "general",
+    "--parent_subroutine",
+    "test_procedures",
+    "--target_subroutines",
+    "set_data",
+    "display_data",
+    "dump_to_file",
+    "process_data",
+    "scale_data",
+    "reset_data",
+    "compute_average",
+    "--openacc",
+    "False",
+    "--f2py",
+    "False",
+    "--py2jx",
+    "False",
+    "--tapenade",
+    "False",
+    "--mode",
+    "jax",
+    "--benchmark_dir",
+    "/tmp/benchmark",
+    "--config_path",
+    "/home/kardaneh/Fgpt/template.yaml",
+    "--vectorize",
+    "kjpindex",
+    "nvm",
+    "npts",
+]
+
+args = parse_args()
+
+isolator = Isolator(
+    rest_of_path=args.rest_of_path,
+    target_model=args.target_model,
+    target_module=args.target_module,
+    work=args.work,
+    config_path=args.config_path,
+    openacc=args.openacc,
+    tapenade=args.tapenade,
+    f2py=args.f2py,
+    py2jx=args.py2jx,
+)
+
+isolator.run(
+    benchmark_dir=args.benchmark_dir,
+    vectorize=args.vectorize,
+    mode=args.mode,
+    parent_subroutine=args.parent_subroutine,
+    target_subroutines=args.target_subroutines,
+)
